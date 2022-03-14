f259da4b6b4cab4ad27fc7a0e481285c8f059179
JobbyJabber <kijowski.hub@gmail.com>
2022-03-14 19:33:38 +0100

    final version of gits Api project

--- get-commits-scirpt.py
+++ get-commits-scirpt.py
@@ -1,69 +1,46 @@
-import getopt
+import argparse
 import json
-import sys
-import time
 from tinydb import TinyDB, Query, where
 from urllib.request import urlopen
-
-
-def instruction():
-    print('''
-    ________________________________________________________________
-    HI! 
-    Its script which saves repo of desired user to tinyDB, 
-    Whenever you try to run script you need to provide parameters: -u username -r reponame
- 
-    Proper executing: ./yourScript.py -u JobbyJabber -r Python-Exercises -b branch
-   
-    - There is a problem when providing exception for http error. Might show exception instead of crashing while URL dosen't exist.
-
-    Author GitID: @JobbyJabber
-    ________________________________________________________________
-    ''')
+from urllib.error import URLError
 
 def main():
-    instruction()
-    userName = ''
-    repoName = ''
-    branchName = ''
-    try: 
-        params, args = getopt.getopt(sys.argv[1:], "u:r:b:")
-        for p, pa in params:
-            if p == '-u':
-                userName = pa
-            if p == '-r':
-                repoName = pa
-            if p == '-b':
-                branchName = pa
-            if p != '-b':
-                branchName = "main"
-            else:
-                instruction()
-    except getopt.GetoptError as e: 
-        print(str(e))
-    checkParameters(userName, repoName)
+
+    parser = argparse.ArgumentParser(description="Process some")
+
+    parser.add_argument('userName', metavar="u", type=str, help='Git UserName')
+    parser.add_argument('repoName', metavar="r", type=str, help='Git RepoName')
+    parser.add_argument('branchName', metavar="b", type=str, help='Git BranchName')
+    
+    args = parser.parse_args()
+    userName = args.userName
+    repoName = args.repoName
+    branchName = args.branchName 
     getCommits(userName, repoName, branchName)
 
-def checkParameters(user, repo):
-    if type(user) != str or len(user) <= 0:
-        print("Please use -u then enter GitHub username ")
-        sys.exit()
-    if type(repo) != str or len(repo) <= 0:
-        print("Please use -r then enter your repo ")
-        sys.exit()
-
+    
 
 def getCommits(user, repo, branch):
-    db = TinyDB('bazaDanych14.json')
+
+    try:
+        api = urlopen(f"https://api.github.com/repos/{user}/{repo}/commits?sha={branch}")
+    except URLError as Error:
+        print("Such address doesnt exist")
+        exit()   
+
+    db = TinyDB('bazaDanych3.json')
 
     repoTable = db.table("REPO")
     userTable = db.table("USER")
     branchTable = db.table("BRANCH")
     otherTable = db.table("OTHER")
+
     w = Query()
+
     result1 = repoTable.search(w.RepoName == repo)
     result2 = userTable.search(w.UserName == user)
     result3 = branchTable.search(w.BranchName == branch)
+
     if len(result1) == 0:    
         repoTable.insert({'RepoName': repo})
     if len(result2) == 0:         
@@ -71,21 +48,18 @@ def getCommits(user, repo, branch):
     if len(result3) == 0: 
         branchTable.insert({'BranchName': branch})
 
-    api = urlopen(f"https://api.github.com/repos/{user}/{repo}/commits")
+    commits = json.loads(api.readline()) 
 
-    commits = json.loads(api.readline())
     for commit in commits: 
         sha = commit['sha']
         q = Query()
         result = otherTable.search(q.sha == sha)
-        if len(result) == 0:         
-            otherTable.insert({'sha': sha})
-            committer = commit["commit"]["committer"]
+        if len(result) == 0: 
+            committer = commit["commit"]["committer"]  
             message = commit['commit']['message']
-            otherTable.insert({'committer': committer, 'message': message})
-            time.sleep(0.1)
-            print(f"\nrepo: {repo} \nbranch: {branch} \nsha: {sha} \nmessage: {message} \ncommitter: {committer}")
-
+            otherTable.insert({'RepoName': repo, 'BranchName': branch, 'UserName': user, 'sha': sha, 'message': message, 'committer': committer })
+            print(f"\nrepo: {repo} \nbranch: {branch} \nsha: {sha} \nmessage: {message} \ncommitter: {committer} ")
+            
 if __name__ == "__main__":
     main()
 
